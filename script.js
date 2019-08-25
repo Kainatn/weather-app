3d47e8729bccb6b40bdb743c2551372c64c45092
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-08-26 00:00:42 +0500

    Weather App WIP

--- script.js
+++ script.js
@@ -8,16 +8,13 @@
 
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5651b311f4bde3b478a18fbf9b55efcd`)
     .then(function (response) {
-      // handle success
-     // console.log(response);
-
-    
-    document.querySelector("h1").innerHTML = response.data.weather[0].main;
+      // handle success  
+    document.querySelector("p").innerHTML = response.data.weather[0].main;
+    document.querySelector('h2').innerHTML = `City: ${response.data.name}`;
+     document.querySelector('h3').innerHTML = `Now: ${response.data.weather[0].description}`
       
     })
     .catch(function (error) {
-      // handle error
-      //console.log(JSON.stringify(error));
       alert("City not found");
     })
     .finally(function () {
